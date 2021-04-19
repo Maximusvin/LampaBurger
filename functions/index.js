028f0f09c98aae48d8804df93aeefd3f5aa68002
Maxim Kozlov <Maximusvin@gmail.com>
2021-04-19 17:17:29 +0300

    added nodemailer

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,34 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const nodemailer = require('nodemailer');
+const htmlToText = require('nodemailer-html-to-text').htmlToText;
+const { email, password } = require('./config');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: email,
+    pas: password,
+  },
+});
+
+transporter.use('compile', htmlToText());
+
+const sendOrderEmail = order => {
+  const options = {
+    from: `LampaBurger`,
+    to: email,
+    subject: `Ваш заказ из LampaBurger`,
+    html: `
+    <div>
+        <h2>Здравствуйте!</h2>
+        <h3>Ваш заказ: </h3>
+    </div>`,
+  };
+
+  transporter.sendMail(options);
+};
+
+exports.sendUserEmail = functions.database
+  .ref('orders/{pushID}')
+  .onCreate(order => sendOrderEmail(order.val()));
