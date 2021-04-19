9cebb99cbeb8dfc76ed24e9d3f127520a35cb3a3
Maxim Kozlov <Maximusvin@gmail.com>
2021-04-19 18:12:02 +0300

    fix config to functions firebase

--- functions/index.js
+++ functions/index.js
@@ -1,29 +1,41 @@
 const functions = require('firebase-functions');
-
 const nodemailer = require('nodemailer');
 const htmlToText = require('nodemailer-html-to-text').htmlToText;
 const { email, password } = require('./config');
 
+//transporter
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: email,
-    pas: password,
+    pass: password,
   },
 });
 
 transporter.use('compile', htmlToText());
 
-const sendOrderEmail = order => {
+const sendOrderEmail = data => {
   const options = {
-    from: `LampaBurger`,
-    to: email,
-    subject: `Ваш заказ из LampaBurger`,
+    from: `BetweenTheBuns ${email}`,
+    to: data.email,
+    subject: 'Ваши заказ из LampaBurger',
     html: `
     <div>
-        <h2>Здравствуйте!</h2>
-        <h3>Ваш заказ: </h3>
-    </div>`,
+      <h2>Здравствуйте, ${data.nameClient}</h2>
+      <h3>Ваш заказ:</h3>
+      <ul>
+        ${data.order.map(
+          ({ name, count, price }) =>
+            `<li>${name} - ${count}шт., цена ${price * count} грн.</li>`,
+        )}
+      </ul>
+      <p>Итого: ${data.order.reduce(
+        (sum, item) => sum + (item.price + item.count),
+        0,
+      )} грн.</p>
+        <small>Ожидайте курьера.</small>
+    </div>
+      `,
   };
 
   transporter.sendMail(options);
